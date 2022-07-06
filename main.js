c75b5ed3e434b56623df8c98aa57dc8fe62c2613
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2022-07-06 18:05:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 function displayTime(){
     var dateTime = new Date();
 
+    var day;
+    switch (dateTime.getDay()) {
+        case 0:
+            day = "Sunday,";
+            break;
+        case 1:
+            day = "Monday,";
+            break;
+        case 2:
+            day = "Tuesday,";
+            break;
+        case 3:
+            day = "Wednesday,";
+            break;
+        case 4:
+            day = "Thursday,";
+            break;
+        case 5:
+            day = "Friday,";
+            break;
+        case 6:
+            day = "Saturday,";
+            break;
+    }
+
     var date = dateTime.getDate();
     var mon = dateTime.getMonth()+1;
     var year = dateTime.getFullYear();
 
+    document.getElementById('day').innerHTML = day;
     document.getElementById('date').innerHTML = date;
     document.getElementById('month').innerHTML = mon;
     document.getElementById('year').innerHTML = year;
